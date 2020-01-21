724faaef950c2e8468865627572844e683b16b35
Jiahao Liu <jiahao.liu@dnata.com>
2020-01-21 16:50:01 +0400

    Created the first version of the app

--- app/src/main/java/com/jiahaoliuliu/isplayservicesavailable/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/isplayservicesavailable/MainActivity.kt
@@ -2,11 +2,34 @@ package com.jiahaoliuliu.isplayservicesavailable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.text)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        text.text = isGooglePlayServicesAvailable().toString()
+    }
+
+    private fun isGooglePlayServicesAvailable(): Boolean {
+        val googleApiAvailability = GoogleApiAvailability.getInstance()
+        val status = googleApiAvailability.isGooglePlayServicesAvailable(this)
+        if (status != ConnectionResult.SUCCESS) {
+            if (googleApiAvailability.isUserResolvableError(status)) {
+                googleApiAvailability.getErrorDialog(this, status, 2404).show()
+            }
+            return false
+        }
+        return true
     }
 }
